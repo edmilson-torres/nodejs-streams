8bcca99cd78c2bc0becdec975d362dce2077727e
Edmilson Torres <edmilson.torres@gmail.com>
2022-12-11 09:11:17 -0300

    refactor: api server setup

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,11 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
+
 import routes from './routers';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', routes);
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (!err) {
-        return next();
-    }
-    res.status(500);
-    res.send({ error: 'Internal server error' });
-});
+app.use('/', routes);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from './app';
 import env from './config/env';
 
-app.listen(env.port, () =>
-    console.log(`Server running at http://localhost:${env.port}`)
-);
+app.listen(env.port, () => {
+    console.log(
+        `[\x1b[34mSERVER\x1b[37m] Server running at \x1b[36mhttp://localhost:${env.port} 🤖  \x1b`
+    );
+});
