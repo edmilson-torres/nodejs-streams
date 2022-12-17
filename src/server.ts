cf053d4c749208d8eb47b5af1f9caab01e0bf9c3
Edmilson Torres <edmilson.torres@gmail.com>
2022-12-17 10:47:00 -0300

    refactor: terminal color fix

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import env from './config/env';
 
 app.listen(env.port, () => {
     console.log(
-        `[\x1b[34mSERVER\x1b[37m] Server running at \x1b[36mhttp://localhost:${env.port} 🤖  \x1b`
+        `[\x1b[34mSERVER\x1b[37m] Server running at \x1b[36mhttp://localhost:${env.port} 🤖  \x1b[37m`
     );
 });
